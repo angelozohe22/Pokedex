eda89fff5c47c40541b2ef2026f15cbcef8f0dd2
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-03-13 00:23:39 -0500

    [Feat][PDKit] Add PDKit framework

---
+++ PDKit/PDKit/PDKit.h
@@ -0,0 +1,18 @@
+//
+//  PDKit.h
+//  PDKit
+//
+//  Created by Luis Angel Inga Mendoza on 13/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PDKit.
+FOUNDATION_EXPORT double PDKitVersionNumber;
+
+//! Project version string for PDKit.
+FOUNDATION_EXPORT const unsigned char PDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDKit/PublicHeader.h>
+
+
