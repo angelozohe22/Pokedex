f20aafa8d8179a8d06fbcb6dffae4fccfea74217
Luis Angel Inga Mendoza <lingamen@emeal.nttdata.com>
2023-03-13 13:01:33 -0500

    [Feat][PDDependencies] Add PDDependencies framework

---
+++ PDDependencies/PDDependencies/PDDependencies.h
@@ -0,0 +1,18 @@
+//
+//  PDDependencies.h
+//  PDDependencies
+//
+//  Created by Luis Angel Inga Mendoza on 13/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PDDependencies.
+FOUNDATION_EXPORT double PDDependenciesVersionNumber;
+
+//! Project version string for PDDependencies.
+FOUNDATION_EXPORT const unsigned char PDDependenciesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PDDependencies/PublicHeader.h>
+
+
